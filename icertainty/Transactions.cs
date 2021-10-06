662a507ee8a63d67201991921d833cd1543faa02
Popelier Emiel <emiel.popelier@edu-kw1c.nl>
2021-10-06 15:05:48 +0200

    Task 2

---
+++ icertainty/Transactions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace icertainty
+{
+    public class Transactions
+    {
+        public static string[] GroupTransactions(string[] transactions)
+        {
+            var groupedTransactions = transactions.GroupBy(transaction => transaction)
+                                 .ToDictionary(item => item.Key, item => item.Count())
+                                 .OrderByDescending(item => item.Value)
+                                 .ThenBy(item => item.Key)
+                                 .Select(transaction => transaction.Key + " " + transaction.Value);
+
+            return groupedTransactions.ToArray();
+        }
+    }
+}
---
+++ icertaintyTests/TransactionsTests.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using icertainty;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace icertainty.Tests
+{
+    [TestClass()]
+    public class TransactionsTests
+    {
+        [TestMethod()]
+        public void GroupTransactionsTest1()
+        {
+            string[] transactions = { "notebook", "notebook", "mouse", "keyboard", "mouse" };
+            string[] result = new[] { "mouse 2", "notebook 2", "keyboard 1" };
+
+            string[] grouped = Transactions.GroupTransactions(transactions);
+
+            CollectionAssert.AreEqual(grouped, result);
+        }
+
+        [TestMethod()]
+        public void GroupTransactionsTest2()
+        {
+            string[] transactions = { "notebook", "keyboard", "mouse", "mouse", "notebook", "mouse" };
+            string[] result = new[] { "mouse 3", "notebook 2", "keyboard 1" };
+
+            string[] grouped = Transactions.GroupTransactions(transactions);
+
+            CollectionAssert.AreEqual(grouped, result);
+        }
+
+        [TestMethod()]
+        public void GroupTransactionsTest3()
+        {
+            string[] transactions = { "notebook", "notebook", "mouse", "monitor", "monitor", "mouse", "keyboard", "notebook", "monitor" };
+            string[] result = new[] { "monitor 3", "notebook 3", "mouse 2", "keyboard 1"};
+
+            string[] grouped = Transactions.GroupTransactions(transactions);
+
+
+            CollectionAssert.AreEqual(grouped, result);
+        }
+
+    }
+}
